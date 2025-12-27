bd5b58df198b98da2897a6a7d78158987e55d943
Shubham Patel <shubhampatel1573@gmail.com>
2025-12-27 22:08:49 +0530

    implemented router.route for user

--- routes/user.js
+++ routes/user.js
@@ -6,13 +6,13 @@ const { saveRedirectUrl } = require("../middleware");
 
 const userController = require("../controllers/users");
 
-router.get("/signup", userController.renderSignupForm);
+router.route("/signup")
+.get(userController.renderSignupForm)
+.post(wrapAsync(userController.signup))
 
-router.post("/signup", wrapAsync(userController.signup));
-
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), wrapAsync(userController.login))
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), wrapAsync(userController.login))
 
 router.get("/logout", userController.logout)
 
